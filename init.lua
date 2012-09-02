406fdacfbb5d73f7afcd9b309a8d964eaa9a1a51
Maciej Kasatkin <mk@realbadangel.pl>
2012-09-02 03:46:00 +0200

    too soon for generator ;)

--- init.lua
+++ init.lua
@@ -31,7 +31,6 @@ dofile(minetest.get_modpath("technic").."/grinder.lua")
 dofile(minetest.get_modpath("technic").."/mining_laser_mk1.lua")
 --dofile(minetest.get_modpath("technic").."/project_table.lua")
 --dofile(minetest.get_modpath("technic").."/injector.lua")
-dofile(minetest.get_modpath("technic").."/generator.lua")
 dofile(minetest.get_modpath("technic").."/solar_panel.lua")
 
 
