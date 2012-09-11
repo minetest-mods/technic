243028dfd5679769c86a1f0bff8ad1c18bc6e2ed
Maciej Kasatkin <mk@realbadangel.pl>
2012-09-11 14:39:10 +0200

    Added craftin recipes for pneumatic tubes

--- items.lua
+++ items.lua
@@ -9,3 +9,12 @@ minetest.register_craftitem( "technic:doped_silicon_wafer", {
 	inventory_image = "technic_doped_silicon_wafer.png",
 	on_place_on_ground = minetest.craftitem_place_item,
 })
+
+minetest.register_craft({
+	output = 'pipeworks:tube_000000 8',
+	recipe = {
+		{'technic:stainless_steel_ingot', 'default:glass', 'technic:stainless_steel_ingot'},
+		{'technic:stainless_steel_ingot', 'default:glass', 'technic:stainless_steel_ingot'},
+		{'technic:stainless_steel_ingot', 'default:glass', 'technic:stainless_steel_ingot'},
+	}
+})
