2d853d264e960860cd2cd8071a06ee6694958733
Maciej Kasatkin <mk@realbadangel.pl>
2012-10-30 03:50:49 +0100

    Added regular Alloy Furnace

--- alloy_furnace.lua
+++ alloy_furnace.lua
@@ -30,8 +30,6 @@ register_alloy_recipe ("technic:copper_ingot",2, "technic:zinc_ingot",1, "techni
 register_alloy_recipe ("default:sand",2, "technic:coal_dust",2, "technic:silicon_wafer",1)
 register_alloy_recipe ("technic:silicon_wafer",1, "technic:mithril_dust",1, "technic:doped_silicon_wafer",1)
 
-minetest.register_alias("alloy_furnace", "technic:alloy_furnace")
-
 
 minetest.register_craft({
 	output = 'technic:alloy_furnace',
@@ -50,7 +48,7 @@ alloy_furnace_formspec =
 	"list[current_name;src2;3,2;1,1;]"..
 	"list[current_name;dst;5,1;2,2;]"..
 	"list[current_player;main;0,5;8,4;]"..
-	"label[0,0;Alloy Furnace]"..
+	"label[0,0;Electric Alloy Furnace]"..
 	"label[1,3;Power level]"
 	
 minetest.register_node("technic:alloy_furnace", {
@@ -68,7 +66,7 @@ minetest.register_node("technic:alloy_furnace", {
 		local meta = minetest.env:get_meta(pos)
 		meta:set_float("technic_power_machine", 1)
 		meta:set_string("formspec", alloy_furnace_formspec)
-		meta:set_string("infotext", "Alloy furnace")
+		meta:set_string("infotext", "Electric Alloy furnace")
 		local inv = meta:get_inventory()
 		inv:set_size("src", 1)
 		inv:set_size("src2", 1)
@@ -106,18 +104,6 @@ minetest.register_node("technic:alloy_furnace_active", {
 	internal_EU_buffer=0;
 	interal_EU_buffer_size=2000;
 	technic_power_machine=1,
-	on_construct = function(pos)
-		local meta = minetest.env:get_meta(pos)
-		meta:set_float("technic_power_machine", 1)
-		meta:set_string("formspec", alloy_furnace_formspec)
-		meta:set_string("infotext", "Alloy furnace");
-		local inv = meta:get_inventory()
-		inv:set_size("src", 1)
-		inv:set_size("dst", 4)
-		local EU_used  = 0
-		local furnace_is_cookin = 0
-		local cooked = nil
-	end,
 	can_dig = function(pos,player)
 		local meta = minetest.env:get_meta(pos);
 		local inv = meta:get_inventory()
@@ -149,7 +135,7 @@ minetest.register_abm({
 				"list[current_name;src2;3,2;1,1;]"..
 				"list[current_name;dst;5,1;2,2;]"..
 				"list[current_player;main;0,5;8,4;]"..
-				"label[0,0;Alloy Furnace]"..
+				"label[0,0;Electric Alloy Furnace]"..
 				"label[1,3;Power level]")
 
 		local inv = meta:get_inventory()
@@ -210,7 +196,7 @@ minetest.register_abm({
 		
 		if dst_index and meta:get_int("furnace_is_cookin")==0 then
 			hacky_swap_node(pos,"technic:alloy_furnace_active")
-			meta:set_string("infotext","Alloy Furnace active")
+			meta:set_string("infotext","Electric Alloy Furnace active")
 			meta:set_int("furnace_is_cookin",1)
 			meta:set_string("src_time", 0)
 			return
@@ -219,7 +205,7 @@ minetest.register_abm({
 			
 		if meta:get_int("furnace_is_cookin")==0 or dst_index==nil then
 			hacky_swap_node(pos,"technic:alloy_furnace")
-			meta:set_string("infotext","Alloy Furnace inactive")
+			meta:set_string("infotext","Electric Alloy Furnace inactive")
 			meta:set_int("furnace_is_cookin",0)
 			meta:set_string("src_time", 0)
 		end
@@ -238,3 +224,201 @@ if	alloy_recipes[i].src1_name==src_item1["name"] and
 end
 return nil
 end
+
+
+--coal driven alloy furnace:
+
+minetest.register_craft({
+	output = 'technic:coal_alloy_furnace',
+	recipe = {
+		{'default:brick', 'default:brick', 'default:brick'},
+		{'default:brick', '', 'default:brick'},
+		{'default:brick', 'default:brick', 'default:brick'},
+	}
+})
+
+
+coal_alloy_furnace_formspec =
+	"size[8,9]"..
+	"label[0,0;Alloy Furnace]"..
+	"image[2,2;1,1;default_furnace_fire_bg.png]"..
+	"list[current_name;fuel;2,3;1,1;]"..
+	"list[current_name;src;2,1;1,1;]"..
+	"list[current_name;src2;3,1;1,1;]"..
+	"list[current_name;dst;5,1;2,2;]"..
+	"list[current_player;main;0,5;8,4;]"
+	
+minetest.register_node("technic:coal_alloy_furnace", {
+	description = "Alloy Furnace",
+	tiles = {"technic_coal_alloy_furnace_top.png", "technic_coal_alloy_furnace_bottom.png", "technic_coal_alloy_furnace_side.png",
+		"technic_coal_alloy_furnace_side.png", "technic_coal_alloy_furnace_side.png", "technic_coal_alloy_furnace_front.png"},
+	paramtype2 = "facedir",
+	groups = {cracky=2},
+	legacy_facedir_simple = true,
+	sounds = default.node_sound_stone_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.env:get_meta(pos)
+		meta:set_string("formspec", coal_alloy_furnace_formspec)
+		meta:set_string("infotext", "Alloy Furnace")
+		local inv = meta:get_inventory()
+		inv:set_size("fuel", 1)
+		inv:set_size("src", 1)
+		inv:set_size("src2", 1)
+		inv:set_size("dst", 4)
+		local furnace_is_cookin = 0
+		local dst_index = nil
+
+	end,
+	can_dig = function(pos,player)
+		local meta = minetest.env:get_meta(pos);
+		local inv = meta:get_inventory()
+		if not (inv:is_empty("fuel") or inv:is_empty("dst") or inv:is_empty("src") or inv:is_empty("src2") )then
+			return false
+			end
+		return true
+	end,
+})
+
+minetest.register_node("technic:coal_alloy_furnace_active", {
+	description = "Alloy Furnace",
+	tiles = {"technic_coal_alloy_furnace_top.png", "technic_coal_alloy_furnace_bottom.png", "technic_coal_alloy_furnace_side.png",
+		"technic_coal_alloy_furnace_side.png", "technic_coal_alloy_furnace_side.png", "technic_coal_alloy_furnace_front_active.png"},
+	paramtype2 = "facedir",
+	light_source = 8,
+	drop = "technic:coal_alloy_furnace",
+	groups = {cracky=2, not_in_creative_inventory=1},
+	legacy_facedir_simple = true,
+	sounds = default.node_sound_stone_defaults(),
+	can_dig = function(pos,player)
+		local meta = minetest.env:get_meta(pos);
+		local inv = meta:get_inventory()
+		if not (inv:is_empty("fuel") or inv:is_empty("dst") or inv:is_empty("src") or inv:is_empty("src2") )then
+			return false
+			end
+		return true
+	end,
+})
+
+minetest.register_abm({
+	nodenames = {"technic:coal_alloy_furnace","technic:coal_alloy_furnace_active"},
+	interval = 1,
+	chance = 1,
+	
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local meta = minetest.env:get_meta(pos)
+		for i, name in ipairs({
+				"fuel_totaltime",
+				"fuel_time",
+				"src_totaltime",
+				"src_time"
+		}) do
+			if meta:get_string(name) == "" then
+				meta:set_float(name, 0.0)
+			end
+		end
+
+		local inv = meta:get_inventory()
+
+		srcstack = inv:get_stack("src", 1)
+		if srcstack then src_item1=srcstack:to_table() end
+		srcstack = inv:get_stack("src2", 1)
+		if srcstack then src_item2=srcstack:to_table() end
+		dst_index=nil
+
+		if src_item1 and src_item2 then 
+				dst_index=get_cook_result(src_item1,src_item2) 
+				end	
+		
+		local was_active = false
+		
+		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
+			was_active = true
+			meta:set_float("fuel_time", meta:get_float("fuel_time") + 1)
+			meta:set_float("src_time", meta:get_float("src_time") + 1)
+			if dst_index and meta:get_float("src_time") >= 5 then
+				-- check if there's room for output in "dst" list
+				dst_stack={}
+				dst_stack["name"]=alloy_recipes[dst_index].dst_name
+				dst_stack["count"]=alloy_recipes[dst_index].dst_count			
+				if inv:room_for_item("dst",dst_stack) then
+					-- Put result in "dst" list
+					inv:add_item("dst", dst_stack)
+					-- take stuff from "src" list
+					for i=1,alloy_recipes[dst_index].src1_count,1 do
+						srcstack = inv:get_stack("src", 1)
+						srcstack:take_item()
+						inv:set_stack("src", 1, srcstack)
+						end
+					for i=1,alloy_recipes[dst_index].src2_count,1 do
+						srcstack = inv:get_stack("src2", 1)
+						srcstack:take_item()
+						inv:set_stack("src2", 1, srcstack)
+						end
+				else
+					print("Furnace inventory full!")
+				end
+				meta:set_string("src_time", 0)
+			end
+		end
+		
+		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
+			local percent = math.floor(meta:get_float("fuel_time") /
+					meta:get_float("fuel_totaltime") * 100)
+			meta:set_string("infotext","Furnace active: "..percent.."%")
+			hacky_swap_node(pos,"technic:coal_alloy_furnace_active")
+			meta:set_string("formspec",
+				"size[8,9]"..
+				"label[0,0;Electric Alloy Furnace]"..
+				"image[2,2;1,1;default_furnace_fire_bg.png^[lowpart:"..
+						(100-percent)..":default_furnace_fire_fg.png]"..
+				"list[current_name;fuel;2,3;1,1;]"..
+				"list[current_name;src;2,1;1,1;]"..
+				"list[current_name;src2;3,1;1,1;]"..
+				"list[current_name;dst;5,1;2,2;]"..
+				"list[current_player;main;0,5;8,4;]")
+			return
+		end
+
+		local fuel = nil
+		local fuellist = inv:get_list("fuel")
+		
+		srcstack = inv:get_stack("src", 1)
+		if srcstack then src_item1=srcstack:to_table() end
+		srcstack = inv:get_stack("src2", 1)
+		if srcstack then src_item2=srcstack:to_table() end
+		dst_index=nil
+
+		if src_item1 and src_item2 then 
+				dst_index=get_cook_result(src_item1,src_item2) 
+				end
+		
+		
+		if fuellist then
+			fuel = minetest.get_craft_result({method = "fuel", width = 1, items = fuellist})
+		end
+
+		if fuel.time <= 0 then
+			meta:set_string("infotext","Furnace out of fuel")
+			hacky_swap_node(pos,"technic:coal_alloy_furnace")
+			meta:set_string("formspec", coal_alloy_furnace_formspec)
+			return
+		end
+
+		if dst_index==nil then
+			if was_active then
+				meta:set_string("infotext","Furnace is empty")
+				hacky_swap_node(pos,"technic:coal_alloy_furnace")
+				meta:set_string("formspec", coal_alloy_furnace_formspec)
+			end
+			return
+		end
+
+		meta:set_string("fuel_totaltime", fuel.time)
+		meta:set_string("fuel_time", 0)
+		
+		local stack = inv:get_stack("fuel", 1)
+		stack:take_item()
+		inv:set_stack("fuel", 1, stack)
+	
+end,		
+})
