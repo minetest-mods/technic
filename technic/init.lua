5d470cd753efe8f4640099165a7bfc0c6e181c35
ShadowNinja <noreply@gmail.com>
2013-07-11 12:19:09 -0400

    Move files to subfolders

--- technic/init.lua
+++ technic/init.lua
@@ -4,13 +4,15 @@
 
 technic = {}
 
+local modpath = minetest.get_modpath("technic")
+
+technic.modpath = modpath
+
 technic.dprint = function(string)
 	if technic.DBG == 1 then
 		print(string)
 	end
 end
-
-local modpath = minetest.get_modpath("technic")
 
 --Read technic config file
 dofile(modpath.."/config.lua")
@@ -22,65 +24,9 @@ dofile(modpath.."/items.lua")
 
 -- Register functions
 dofile(modpath.."/register_machine_and_tool.lua")
-dofile(modpath.."/alloy_furnaces_commons.lua") -- Idea: Let the LV, MV, HV version of the furnace support different alloys
 
--- Switching station LV,MV,HV
-dofile(modpath.."/switching_station.lua")
-dofile(modpath.."/supply_converter.lua")
-
---LV machines
-dofile(modpath.."/wires.lua")
-dofile(modpath.."/battery_box.lua")
-dofile(modpath.."/alloy_furnace.lua")
-dofile(modpath.."/solar_panel.lua")
-dofile(modpath.."/solar_array_lv.lua")
-dofile(modpath.."/geothermal.lua")
-dofile(modpath.."/water_mill.lua")
-dofile(modpath.."/generator.lua")
-dofile(modpath.."/electric_furnace.lua")
-dofile(modpath.."/tool_workshop.lua")
-dofile(modpath.."/music_player.lua")
-dofile(modpath.."/grinder.lua")
-dofile(modpath.."/cnc.lua")
-dofile(modpath.."/cnc_api.lua")
-dofile(modpath.."/cnc_nodes.lua")
-dofile(modpath.."/extractor.lua")
-
---MV machines
-dofile(modpath.."/wires_mv.lua")
-dofile(modpath.."/battery_box_mv.lua")
-dofile(modpath.."/solar_array_mv.lua")
---dofile(modpath.."/down_converter_mv.lua")
-dofile(modpath.."/electric_furnace_mv.lua")
-dofile(modpath.."/alloy_furnace_mv.lua")
-dofile(modpath.."/forcefield.lua")
----- The power radiator supplies appliances with inductive coupled power:
----- lighting and associated textures is taken directly from VanessaE's homedecor and made electric.
-dofile(modpath.."/power_radiator.lua")
-dofile(modpath.."/lighting.lua")
-
---HV machines
-dofile(modpath.."/wires_hv.lua")
-dofile(modpath.."/battery_box_hv.lua")
-dofile(modpath.."/solar_array_hv.lua")
---dofile(modpath.."/down_converter_hv.lua")
-dofile(modpath.."/nuclear_reactor_hv.lua")
-
---Tools
-if technic.config:getBool("enable_mining_drill") then dofile(modpath.."/mining_drill.lua") end
-if technic.config:getBool("enable_mining_laser") then dofile(modpath.."/mining_laser_mk1.lua") end
-if technic.config:getBool("enable_flashlight") then dofile(modpath.."/flashlight.lua") end
-dofile(modpath.."/cans.lua")
-dofile(modpath.."/chainsaw.lua")
-dofile(modpath.."/tree_tap.lua")
-dofile(modpath.."/sonic_screwdriver.lua")
-
----- mesecons and tubes related
-dofile(modpath.."/injector.lua")
-dofile(modpath.."/node_breaker.lua")
-dofile(modpath.."/deployer.lua")
-dofile(modpath.."/constructor.lua")
-dofile(modpath.."/frames.lua")
+-- Machines
+dofile(modpath.."/machines/init.lua")
 
 function has_locked_chest_privilege(meta, player)
    if player:get_player_name() ~= meta:get_string("owner") then
---
+++ technic/machines/init.lua
@@ -0,0 +1,10 @@
+local path = technic.modpath.."/machines"
+
+dofile(path.."/switching_station.lua")
+dofile(path.."/supply_converter.lua")
+dofile(path.."/alloy_furnaces_commons.lua")
+dofile(path.."/lv/init.lua")
+dofile(path.."/mv/init.lua")
+dofile(path.."/hv/init.lua")
+dofile(path.."/other/init.lua")
+
