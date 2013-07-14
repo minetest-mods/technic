25e1b995eb600a6fd430f5c11897686f0c6cf312
pagliaccio <mangheriniroberto@alice.it>
2013-07-14 18:03:36 +0200

    add generator MV and bio generator LV

--- technic/machines/mv/init.lua
+++ technic/machines/mv/init.lua
@@ -5,6 +5,7 @@ dofile(path.."/battery_box.lua")
 dofile(path.."/solar_array.lua")
 dofile(path.."/electric_furnace.lua")
 dofile(path.."/alloy_furnace.lua")
+dofile(path.."/generator_mv.lua")
 
 -- The power radiator supplies appliances with inductive coupled power:
 -- Lighting and associated textures is taken directly from VanessaE's homedecor and made electric.
