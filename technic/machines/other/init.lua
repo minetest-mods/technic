d8ec3f8bbee5c3c489e4c5740fc8c84449eb86c3
RealBadAngel <mk@realbadangel.pl>
2013-07-25 07:59:00 +0200

    Deployer and node breaker are now moved to pipeworks.

--- technic/machines/other/init.lua
+++ technic/machines/other/init.lua
@@ -2,7 +2,5 @@ local path = technic.modpath.."/machines/other"
 
 -- mesecons and tubes related
 dofile(path.."/injector.lua")
-dofile(path.."/node_breaker.lua")
-dofile(path.."/deployer.lua")
 dofile(path.."/constructor.lua")
 dofile(path.."/frames.lua")
