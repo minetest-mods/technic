a92bb9724104f4628946453a5cbbb98eb02a1e7d
SmallJoker <st.rentsch@hotmail.com>
2013-12-03 19:51:31 +0100

    Fixed possible filling bugs

--- technic/tools/cans.lua
+++ technic/tools/cans.lua
@@ -40,7 +40,7 @@ minetest.register_tool("technic:water_can", {
 			charge = tonumber(itemstack:get_metadata())
 		end
 		if node.name == "default:water_source" then
-			if charge + 1 < 17 then
+			if charge < water_can_max_load then
 				minetest.remove_node(pointed_thing.under)
 				charge = charge + 1
 				itemstack:set_metadata(tostring(charge))
@@ -48,7 +48,7 @@ minetest.register_tool("technic:water_can", {
 			end
 			return itemstack
 		end
-		if charge == 0 then
+		if charge <= 0 then
 			return
 		end
 
@@ -89,7 +89,7 @@ minetest.register_tool("technic:lava_can", {
 		end
 
 		if node.name == "default:lava_source" then
-			if charge + 1 < 17 then
+			if charge < lava_can_max_load then
 				minetest.remove_node(pointed_thing.under)
 				charge = charge + 1
 				itemstack:set_metadata(tostring(charge))
@@ -97,7 +97,7 @@ minetest.register_tool("technic:lava_can", {
 			end
 			return itemstack
 		end
-		if charge == 0 then
+		if charge <= 0 then
 			return
 		end
 
@@ -119,4 +119,3 @@ minetest.register_tool("technic:lava_can", {
 		end
 	end,
 })
-
