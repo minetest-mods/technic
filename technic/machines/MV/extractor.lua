88cbc6c2a70db42018051129cc0cd12832013f93
Novatux <nathanael.courant@laposte.net>
2014-07-02 19:43:24 +0200

    Add MV extractor

---
+++ technic/machines/MV/extractor.lua
@@ -0,0 +1,12 @@
+-- MV extractor
+
+minetest.register_craft({
+	output = 'technic:mv_extractor',
+	recipe = {
+		{'technic:stainless_steel_ingot', 'technic:lv_extractor',   'technic:stainless_steel_ingot'},
+		{'pipeworks:tube_1',              'technic:mv_transformer', 'pipeworks:tube_1'},
+		{'technic:stainless_steel_ingot', 'technic:mv_cable0',      'technic:stainless_steel_ingot'},
+	}
+})
+
+technic.register_extractor({tier = "MV", demand = {800, 600, 400}, speed = 2, upgrade = 1, tube = 1})
