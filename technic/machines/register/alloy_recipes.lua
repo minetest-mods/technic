0003ec6a79eca03f57fdb694724b0f7c7347158e
Zefram <zefram@fysh.org>
2014-08-01 17:40:35 +0100

    Make carbon steel alloying quicker
    Taking the same time per alloying cycle as other alloys meant that carbon
    steel was being produced painfully slowly, becuase it processes much less
    material per cycle than other alloys.  This change halves the cycle time,
    which leaves it still processing less material per second than other
    alloying processes, but by a less drastic margin.

--- technic/machines/register/alloy_recipes.lua
+++ technic/machines/register/alloy_recipes.lua
@@ -14,10 +14,10 @@ end
 local recipes = {
 	{"technic:copper_dust 3",         "technic:tin_dust",           "technic:bronze_dust 4"},
 	{"default:copper_ingot 3",        "moreores:tin_ingot",         "default:bronze_ingot 4"},
-	{"technic:wrought_iron_dust",     "technic:coal_dust",          "technic:carbon_steel_dust"},
-	{"technic:wrought_iron_ingot",    "technic:coal_dust",          "technic:carbon_steel_ingot"},
-	{"technic:carbon_steel_dust",     "technic:coal_dust",          "technic:cast_iron_dust"},
-	{"technic:carbon_steel_ingot",    "technic:coal_dust",          "technic:cast_iron_ingot"},
+	{"technic:wrought_iron_dust",     "technic:coal_dust",          "technic:carbon_steel_dust", 3},
+	{"technic:wrought_iron_ingot",    "technic:coal_dust",          "technic:carbon_steel_ingot", 3},
+	{"technic:carbon_steel_dust",     "technic:coal_dust",          "technic:cast_iron_dust", 3},
+	{"technic:carbon_steel_ingot",    "technic:coal_dust",          "technic:cast_iron_ingot", 3},
 	{"technic:carbon_steel_dust 3",   "technic:chromium_dust",      "technic:stainless_steel_dust 4"},
 	{"technic:carbon_steel_ingot 3",  "technic:chromium_ingot",     "technic:stainless_steel_ingot 4"},
 	{"technic:copper_dust 2",         "technic:zinc_dust",          "technic:brass_dust 3"},
@@ -27,5 +27,5 @@ local recipes = {
 }
 
 for _, data in pairs(recipes) do
-	technic.register_alloy_recipe({input = {data[1], data[2]}, output = data[3]})
+	technic.register_alloy_recipe({input = {data[1], data[2]}, output = data[3], time = data[4]})
 end
