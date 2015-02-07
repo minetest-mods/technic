5ac93cccc9163a65e7e53e714f5e965ada2c070d
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2015-02-07 12:42:18 -0500

    simplify pipeworks tube detect deMorgan's axiom, which I suck at using :P

--- technic/machines/register/battery_box.lua
+++ technic/machines/register/battery_box.lua
@@ -195,8 +195,8 @@ function technic.register_battery_box(data)
 			allow_metadata_inventory_take = technic.machine_inventory_take,
 			allow_metadata_inventory_move = technic.machine_inventory_move,
 			technic_run = run,
-			after_place_node = (not tube) or pipeworks.after_place,
-			after_dig_node = (not tube) or pipeworks.after_dig
+			after_place_node = tube and pipeworks.after_place,
+			after_dig_node = tube and pipeworks.after_dig
 		})
 	end
 
--- technic/machines/register/generator.lua
+++ technic/machines/register/generator.lua
@@ -109,8 +109,8 @@ function technic.register_generator(data)
 		allow_metadata_inventory_take = technic.machine_inventory_take,
 		allow_metadata_inventory_move = technic.machine_inventory_move,
 		technic_run = run,
-		after_place_node = (not tube) or pipeworks.after_place,
-		after_dig_node = (not tube) or pipeworks.after_dig
+		after_place_node = tube and pipeworks.after_place,
+		after_dig_node = tube and pipeworks.after_dig
 	})
 
 	minetest.register_node("technic:"..ltier.."_generator_active", {
