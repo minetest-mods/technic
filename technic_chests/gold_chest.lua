4753cb8fcb7c8ba66d8e193c51caef842ef7bfae
Rogier <rogier777@gmail.com>
2015-02-07 20:42:23 +0100

    Make chests usable without moreores (& add soft dependency)

--- technic_chests/gold_chest.lua
+++ technic_chests/gold_chest.lua
@@ -1,21 +1,31 @@
 
-minetest.register_craft({
-	output = 'technic:gold_chest',
-	recipe = {
-		{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
-		{'default:gold_ingot','technic:silver_chest','default:gold_ingot'},
-		{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
-	}
-})
+local material_list
+if minetest.get_modpath("moreores") then
+	material_list = { 'silver' }
+else
+	-- Make the gold chest obtainable for mere mortals (the silver chest is not obtainable)
+	material_list = { 'copper', 'silver' }
+end
 
-minetest.register_craft({
-	output = 'technic:gold_locked_chest',
-	recipe = {
-		{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
-		{'default:gold_ingot','technic:silver_locked_chest','default:gold_ingot'},
-		{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
-	}
-})
+for _, material in ipairs(material_list) do
+	minetest.register_craft({
+		output = 'technic:gold_chest',
+		recipe = {
+			{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
+			{'default:gold_ingot',"technic:"..material.."_chest",'default:gold_ingot'},
+			{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
+		}
+	})
+
+	minetest.register_craft({
+		output = 'technic:gold_locked_chest',
+		recipe = {
+			{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
+			{'default:gold_ingot',"technic:"..material.."_locked_chest",'default:gold_ingot'},
+			{'default:gold_ingot','default:gold_ingot','default:gold_ingot'},
+		}
+	})
+end
 
 minetest.register_craft({
 	output = 'technic:gold_locked_chest',
