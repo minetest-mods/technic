a861362605c20ea8340dc9af049c5ea491bc9664
runningscissors <steve@sharpsoft.tk>
2015-07-14 17:05:44 -0400

    added multi-tier geothermal

---
+++ technic/machines/LV/oldgeo.lua
@@ -0,0 +1,14 @@
+
+
+---  old non tiered one
+--- A geothermal EU generator
+--- Using hot lava and water this device can create energy from steam
+--- The machine is only producing LV EUs and can thus not drive more advanced equipment
+--- The output is a little more than the coal burning generator (max 300EUs)
+
+
+
+
+
+technic.register_geothermal({tier="LV", power=10, old=true})
+
