8cab1a0aec8359b14824e0d2006b05796862a962
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2017-02-21 13:12:17 -0500

    don't allow standing water to power a gen, and add support for default flowing river water

--- technic/machines/LV/water_mill.lua
+++ technic/machines/LV/water_mill.lua
@@ -17,9 +17,9 @@ minetest.register_craft({
 
 local function check_node_around_mill(pos)
 	local node = minetest.get_node(pos)
-	if node.name == "default:water_flowing" or
-	   node.name == "default:water_source" then
-		return true
+	if node.name == "default:water_flowing"
+	  or node.name == "default:river_water_flowing" then
+		return node.param2 -- returns approx. water flow, if any
 	end
 	return false
 end
