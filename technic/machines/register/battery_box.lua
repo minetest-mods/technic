33455328bd4fbfacd26e3f02b77f2b54bea982d9
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2017-04-12 20:21:20 -0400

    Make batteries only charge/discharge from the bottom (they were already supposed to but would leech charge from sideways neighbors too)

--- technic/machines/register/battery_box.lua
+++ technic/machines/register/battery_box.lua
@@ -133,7 +133,14 @@ function technic.register_battery_box(data)
 	end
 
 	local run = function(pos, node)
+		local below = minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z})
 		local meta           = minetest.get_meta(pos)
+
+		if below.name ~= "technic:"..ltier.."_cable" then
+			meta:set_string("infotext", S("%s Battery Box Has No Network"):format(tier))
+			return
+		end
+
 		local eu_input       = meta:get_int(tier.."_EU_input")
 		local current_charge = meta:get_int("internal_EU_charge")
 
