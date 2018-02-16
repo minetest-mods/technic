ca060ac980b9910c910ff00561521733583fda40
Tanmaya Meher <tanmayameher@users.noreply.github.com>
2018-02-16 13:53:51 +0530

    Update water_mill.lua
    reverting changes that shouldn't be there. Because, this is a manual update PR :) Refer to https://github.com/minetest-mods/technic/pull/412#issuecomment-366080090 and https://github.com/minetest-mods/technic/pull/412#issuecomment-366166972

--- technic/machines/LV/water_mill.lua
+++ technic/machines/LV/water_mill.lua
@@ -1,6 +1,6 @@
 -- A water mill produces LV EUs by exploiting flowing water across it
--- It is a LV EU supplyer and fairly low yield (max 175EUs)
--- It is a little over half as good as the thermal generator.
+--- It is a LV EU supplyer and fairly low yield (max 120EUs)
+--- It is a little under half as good as the thermal generator.
 
 local S = technic.getter
 
@@ -29,9 +29,11 @@ end
 local run = function(pos, node)
 	local meta             = minetest.get_meta(pos)
 	local water_flow       = 0
+	local lava_nodes       = 0
 	local production_level = 0
 	local eu_supply        = 0
-	local max_output       = 35 * 5 -- keeping it around 175 mid value little more than previous 150 :)
+	local max_output       = 35 * 45 -- four param2's at 15 makes 60, cap it lower for "overload protection"
+-- (plus we want the gen to report 100% if three sides have full flow)
 
 	local positions = {
 		{x=pos.x+1, y=pos.y, z=pos.z},
