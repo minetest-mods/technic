3dd8ce7a3d16b53d079d37688a95fdf51135f986
h-v-smacker <hans-von-smacker+github@gmail.com>
2018-05-13 08:38:33 +0300

    bakedclay dyes

--- technic/machines/register/extractor_recipes.lua
+++ technic/machines/register/extractor_recipes.lua
@@ -29,6 +29,53 @@ if minetest.get_modpath("dye") then
 		{"bushes:blueberry",                  unifieddyes and "unifieddyes:magenta_s50 4" or "dye:magenta 4"},
 	}
 
+	if minetest.get_modpath("hunger") and minetest.get_modpath("ethereal") then
+		table.insert(dye_recipes, {"ethereal:willow_twig 12", "technic:aspirin_pill"})
+	end
+	
+	if minetest.get_modpath("farming") then
+		-- Cottonseed oil: a fuel and a potent fertilizer (irl: pesticide) ---
+		-- hemp oil calls for 8 seeds, but extractor recipes are normally twice as potent
+		table.insert(dye_recipes, {"farming:seed_cotton 4", "technic:cottonseed_oil"})
+		
+		-- Dyes ---
+		-- better recipes for farming's crafting methods (twice the output)
+		table.insert(dye_recipes, {"farming:chili_pepper", "dye:red 4"})
+		table.insert(dye_recipes, {"farming:beans", "dye:green 4"})
+		table.insert(dye_recipes, {"farming:grapes", "dye:violet 4"})
+		table.insert(dye_recipes, {"farming:cocoa_beans", "dye:brown 4"})
+		-- Some extra recipes:
+		-- Himalayan rhubarb root can give yellow dye IRL
+		table.insert(dye_recipes, {"farming:rhubarb", "dye:yellow 4"})
+		table.insert(dye_recipes, {"farming:onion", "dye:yellow 4"})
+		table.insert(dye_recipes, {"farming:blueberries", "dye:blue 4"})
+		table.insert(dye_recipes, {"farming:raspberries", "dye:red 4"})
+	end
+	
+	if minetest.get_modpath("ethereal") then
+		table.insert(dye_recipes, {"ethereal:seaweed", "dye:dark_green 6"})
+		table.insert(dye_recipes, {"ethereal:coral2", "dye:cyan 6"})
+		table.insert(dye_recipes, {"ethereal:coral3", "dye:orange 6"})
+		table.insert(dye_recipes, {"ethereal:coral4", "dye:pink 6"})
+		table.insert(dye_recipes, {"ethereal:coral5", "dye:green 6"})
+		table.insert(dye_recipes, {"ethereal:fern", "dye:dark_green 4"})
+		table.insert(dye_recipes, {"ethereal:snowygrass", "dye:grey 4"})
+		table.insert(dye_recipes, {"ethereal:crystalgrass", "dye:blue 4"})
+	end
+	
+	if minetest.get_modpath("bakedclay") then
+		table.insert(dye_recipes, {"bakedclay:delphinium", "dye:cyan 8"})
+		table.insert(dye_recipes, {"bakedclay:thistle", "dye:magenta 8"})
+		table.insert(dye_recipes, {"bakedclay:lazarus", "dye:pink 8"})
+		table.insert(dye_recipes, {"bakedclay:mannagrass", "dye:dark_green 8"})
+	end
+
+	
+	if minetest.get_modpath("bonemeal") then
+		table.insert(dye_recipes, {"bonemeal:bone", "dye:white 8"})
+		table.insert(dye_recipes, {"bonemeal:bonemeal", "dye:white 4"})
+	end
+	
 	for _, data in ipairs(dye_recipes) do
 		technic.register_extractor_recipe({input = {data[1]}, output = data[2]})
 	end
