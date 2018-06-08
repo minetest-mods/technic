b92b60b30f8890578a74e152a6d275eb9bdc5738
Vitaliy <silverunicorn2011@yandex.ru>
2018-06-08 23:00:01 +0300

    Fix wooden chest formspec

--- technic_chests/init.lua
+++ technic_chests/init.lua
@@ -15,3 +15,11 @@ dofile(modpath.."/silver_chest.lua")
 dofile(modpath.."/gold_chest.lua")
 dofile(modpath.."/mithril_chest.lua")
 
+minetest.register_lbm({
+	name = "technic_chests:fix_wooden_chests",
+	nodenames = {"default:chest"},
+	action = function(pos, node)
+		metadata = minetest.get_meta(pos)
+		metadata:set_string("formspec", "")
+	end
+})
