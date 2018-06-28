6664714313773e0a0b6e1d55dd2d89c3c571fe18
SmallJoker <mk939@ymail.com>
2018-06-28 19:49:04 +0200

    Skip interact check for fake players
    Fixes crash related to pipeworks
    default.can_interact_with_node is not official part of the API yet. Using it anyway..

--- technic_chests/common.lua
+++ technic_chests/common.lua
@@ -28,7 +28,9 @@ technic.chests.can_dig = function(pos, player)
 end
 
 local function inv_change(pos, count, player)
-	if not default.can_interact_with_node(player, pos) then
+	-- Skip check for pipeworks (fake player)
+	if minetest.is_player(player) and
+			not default.can_interact_with_node(player, pos) then
 		return 0
 	end
 	return count
