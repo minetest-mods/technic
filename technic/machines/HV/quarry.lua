012b961d8f1d0da55d3ae2bd8999feaa742e6ac1
Thomas Rudin <thomas@rudin.io>
2018-11-09 11:02:22 +0100

    allow quarry to operate in vacuum

--- technic/machines/HV/quarry.lua
+++ technic/machines/HV/quarry.lua
@@ -164,7 +164,7 @@ local function quarry_run(pos, node)
 				for ay = startpos.y, digpos.y+1, -1 do
 					local checkpos = {x=digpos.x, y=ay, z=digpos.z}
 					local checknode = technic.get_or_load_node(checkpos) or minetest.get_node(checkpos)
-					if checknode.name ~= "air" then
+					if checknode.name ~= "air" and checknode.name ~= "vacuum:vacuum" then
 						can_dig = false
 						break
 					end
