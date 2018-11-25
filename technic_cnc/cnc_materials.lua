10e2275eac326d7e4905036a1c75197783042e15
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-11-25 08:13:56 -0500

    allow CNC to work without technic
    minetest.conf technic_cnc_use_technic = false disables
    use of power, returns items instantly, uses only default
    stuff in craft recipe
    
    (defaults to enabled if technic is installed)

--- technic_cnc/cnc_materials.lua
+++ technic_cnc/cnc_materials.lua
@@ -1,91 +1,99 @@
 -- REGISTER MATERIALS AND PROPERTIES FOR NONCUBIC ELEMENTS:
 -----------------------------------------------------------
 
-local S = technic.getter
+local S = technic_cnc.getter
 
 -- DIRT
 -------
-technic.cnc.register_all("default:dirt",
+technic_cnc.register_all("default:dirt",
                 {snappy=2,choppy=2,oddly_breakable_by_hand=3,not_in_creative_inventory=1},
                 {"default_grass.png", "default_dirt.png", "default_grass.png"},
                 S("Dirt"))
 -- WOOD
 -------
-technic.cnc.register_all("default:wood",
+technic_cnc.register_all("default:wood",
                 {snappy=2, choppy=2, oddly_breakable_by_hand=2, not_in_creative_inventory=1},
                 {"default_wood.png"},
                 S("Wooden"))
 -- STONE
 --------
-technic.cnc.register_all("default:stone",
+technic_cnc.register_all("default:stone",
                 {cracky=3, not_in_creative_inventory=1},
                 {"default_stone.png"},
                 S("Stone"))
 -- COBBLE
 ---------
-technic.cnc.register_all("default:cobble",
+technic_cnc.register_all("default:cobble",
                 {cracky=3, not_in_creative_inventory=1},
                 {"default_cobble.png"},
                 S("Cobble"))
 -- BRICK
 --------
-technic.cnc.register_all("default:brick",
+technic_cnc.register_all("default:brick",
                 {cracky=3, not_in_creative_inventory=1},
                 {"default_brick.png"},
                 S("Brick"))
 
 -- SANDSTONE
 ------------
-technic.cnc.register_all("default:sandstone",
+technic_cnc.register_all("default:sandstone",
                 {crumbly=2, cracky=3, not_in_creative_inventory=1},
                 {"default_sandstone.png"},
                 S("Sandstone"))
 
 -- LEAVES
 ---------
-technic.cnc.register_all("default:leaves",
+technic_cnc.register_all("default:leaves",
                 {snappy=2, choppy=2, oddly_breakable_by_hand=3, not_in_creative_inventory=1},
                 {"default_leaves.png"},
                 S("Leaves"))
 -- TREE
 -------
-technic.cnc.register_all("default:tree",
+technic_cnc.register_all("default:tree",
                 {snappy=1, choppy=2, oddly_breakable_by_hand=2, flammable=3, wood=1, not_in_creative_inventory=1},
                 {"default_tree.png"},
                 S("Tree"))
 
--- WROUGHT IRON
----------------
-technic.cnc.register_all("default:steelblock",
-                {cracky=1, level=2, not_in_creative_inventory=1},
-                {"technic_wrought_iron_block.png"},
-                S("Wrought Iron"))
-
 -- Bronze
 --------
-technic.cnc.register_all("default:bronzeblock",
+technic_cnc.register_all("default:bronzeblock",
                 {cracky=1, level=2, not_in_creative_inventory=1},
                 {"default_bronze_block.png"},
                 S("Bronze"))
 
--- Stainless Steel
---------
-technic.cnc.register_all("technic:stainless_steel_block",
-                {cracky=1, level=2, not_in_creative_inventory=1},
-                {"technic_stainless_steel_block.png"},
-                S("Stainless Steel"))
 
--- Marble
-------------
-technic.cnc.register_all("technic:marble",
-                {cracky=3, not_in_creative_inventory=1},
-                {"technic_marble.png"},
-                S("Marble"))
+local steeltex = "default_steel_block.png"
+local steelname = "Steel"
 
--- Granite
-------------
-technic.cnc.register_all("technic:granite",
-                {cracky=1, not_in_creative_inventory=1},
-                {"technic_granite.png"},
-                S("Granite"))
+if technic_cnc.technic_modpath then
+	steeltex = "technic_wrought_iron_block.png"
+	steelname = "Wrought Iron"
 
+	-- Stainless Steel
+	--------
+	technic_cnc.register_all("technic:stainless_steel_block",
+					{cracky=1, level=2, not_in_creative_inventory=1},
+					{"technic_stainless_steel_block.png"},
+					S("Stainless Steel"))
+
+	-- Marble
+	------------
+	technic_cnc.register_all("technic:marble",
+					{cracky=3, not_in_creative_inventory=1},
+					{"technic_marble.png"},
+					S("Marble"))
+
+	-- Granite
+	------------
+	technic_cnc.register_all("technic:granite",
+					{cracky=1, not_in_creative_inventory=1},
+					{"technic_granite.png"},
+					S("Granite"))
+end
+
+-- STEEL
+---------------
+technic_cnc.register_all("default:steelblock",
+				{cracky=1, level=2, not_in_creative_inventory=1},
+				{steeltex},
+				S(steelname))
