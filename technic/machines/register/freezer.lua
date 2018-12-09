5efd09d9354a25969243b4a63abaf796cfd232f7
Weinzierl Stefan <Stefan@Weinzierl-Stefan.de>
2018-12-09 15:43:57 +0100

    Using tab-indent

--- technic/machines/register/freezer.lua
+++ technic/machines/register/freezer.lua
@@ -2,8 +2,8 @@
 local S = technic.getter
 
 function technic.register_freezer(data)
-    data.typename = "freezing"
-    data.machine_name = "freezer"
-    data.machine_desc = S("%s Freezer")
-    technic.register_base_machine(data)
+	data.typename = "freezing"
+	data.machine_name = "freezer"
+	data.machine_desc = S("%s Freezer")
+	technic.register_base_machine(data)
 end
--- technic/machines/register/freezer_recipes.lua
+++ technic/machines/register/freezer_recipes.lua
@@ -4,17 +4,17 @@ local S = technic.getter
 technic.register_recipe_type("freezing", { description = S("Freezing") })
 
 function technic.register_freezer_recipe(data)
-    data.time = data.time or 5
-    technic.register_recipe("freezing", data)
+	data.time = data.time or 5
+	technic.register_recipe("freezing", data)
 end
 
 local recipes = {
-    {"bucket:bucket_water", { "default:ice", "bucket:bucket_empty" } },
-    {"bucket:bucket_river_water", { "default:ice", "bucket:bucket_empty" } },
-    {"default:dirt", "default:dirt_with_snow" }
+	{"bucket:bucket_water", { "default:ice", "bucket:bucket_empty" } },
+	{"bucket:bucket_river_water", { "default:ice", "bucket:bucket_empty" } },
+	{"default:dirt", "default:dirt_with_snow" }
 }
 
 for _, data in pairs(recipes) do
-    technic.register_freezer_recipe({input = {data[1]}, output = data[2]})
+	technic.register_freezer_recipe({input = {data[1]}, output = data[2]})
 end
 
