37dcb2a1014e49cb68a836b8d68d12589aba52cb
Thomas Rudin <thomas@rudin.io>
2019-03-11 21:42:40 +0100

    Revert "remove monitoring code and deps"
    This reverts commit 7b16d4fe870016b5e415585f1a57d44ae882745a.

--- technic/machines/switching_station.lua
+++ technic/machines/switching_station.lua
@@ -4,6 +4,20 @@ technic.networks = {}
 technic.cables = {}
 technic.redundant_warn = {}
 
+local has_monitoring = minetest.get_modpath("monitoring")
+local metric_abm_count
+local metric_abm_latency
+
+if has_monitoring then
+	metric_abm_count = monitoring.counter("technic_switching_station_abm_count",
+		"number of technic switch abm calls")
+
+	metric_abm_latency = monitoring.histogram("technic_switching_station_abm_latency",
+		"latency of the technic switch abm calls",
+	  {0.001, 0.005, 0.01, 0.02, 0.1, 0.5, 1.0})
+end
+
+
 local mesecons_path = minetest.get_modpath("mesecons")
 local digilines_path = minetest.get_modpath("digilines")
 
@@ -242,6 +256,13 @@ minetest.register_abm({
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		if not technic.powerctrl_state then return end
 
+		local timer
+		if has_monitoring then
+			metric_abm_count.inc()
+			timer = metric_abm_latency.timer()
+		end
+
+
 		local t0 	       = minetest.get_us_time()
 		local meta             = minetest.get_meta(pos)
 		local meta1            = nil
@@ -424,6 +445,8 @@ minetest.register_abm({
 				minetest.log("warning", "[technic] [+supply] switching station abm took " .. diff .. " us at " .. minetest.pos_to_string(pos))
 			end
 
+			if timer ~= nil then timer.observe() end
+			return
 		end
 
 		-- If the PR supply is not enough for the RE demand we will discharge the batteries too
@@ -453,6 +476,8 @@ minetest.register_abm({
 				minetest.log("warning", "[technic] [-supply] switching station abm took " .. diff .. " us at " .. minetest.pos_to_string(pos))
 			end
 
+			if timer ~= nil then timer.observe() end
+			return
 		end
 
 		-- If the PR+BA supply is not enough for the RE demand: Power only the batteries
@@ -476,6 +501,8 @@ minetest.register_abm({
 			check_timer(pos, meta, diff)
 			minetest.log("warning", "[technic] switching station abm took " .. diff .. " us at " .. minetest.pos_to_string(pos))
 		end
+
+		if timer ~= nil then timer.observe() end
 
 	end,
 })
