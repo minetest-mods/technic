f27964f018d79676210a8862b18cf07bf31321b1
Panquesito7 <halfpacho@gmail.com>
2020-06-12 13:49:57 -0500

    Remove `node_ownership` support

--- technic/machines/MV/lighting.lua
+++ technic/machines/MV/lighting.lua
@@ -14,20 +14,7 @@ end
 
 function technic_homedecor_node_is_owned(pos, placer)
         local ownername = false
-        if type(IsPlayerNodeOwner) == "function" then -- node_ownership mod
-                if HasOwner(pos, placer) then
-                        if not IsPlayerNodeOwner(pos, placer:get_player_name()) then
-                                if type(getLastOwner) == "function" then -- ...is an old version
-                                        ownername = getLastOwner(pos)
-                                elseif type(GetNodeOwnerName) == "function" then -- ...is a recent version
-                                        ownername = GetNodeOwnerName(pos)
-                                else
-                                        ownername = S("someone")
-                                end
-                        end
-                end
-
-        elseif type(isprotect) == "function" then -- glomie's protection mod
+        if type(isprotect) == "function" then -- glomie's protection mod
                 if not isprotect(5, pos, placer) then
                         ownername = S("someone")
                 end
