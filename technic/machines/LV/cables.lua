df93f5d33548a2609cd47ff2063865055a2c2327
SX <sx@minetest>
2020-10-12 03:20:32 +0300

    #97 Add LV & MV digi cables

--- technic/machines/LV/cables.lua
+++ technic/machines/LV/cables.lua
@@ -12,3 +12,39 @@ minetest.register_craft({
 
 technic.register_cable("LV", 2/16)
 
+if minetest.get_modpath("digilines") then
+
+	local S = technic.getter
+
+	if minetest.get_modpath("digistuff") then
+		minetest.register_craft({
+			output = 'technic:lv_digi_cable 1',
+			type = "shapeless",
+			recipe = {'digistuff:digimese', 'technic:lv_cable'}
+		})
+	else
+		minetest.register_craft({
+			output = 'technic:lv_digi_cable 1',
+			recipe = {
+				{'digilines:wire_std_00000000', 'digilines:wire_std_00000000', 'digilines:wire_std_00000000'},
+				{'digilines:wire_std_00000000', 'technic:lv_cable',            'digilines:wire_std_00000000'},
+				{'digilines:wire_std_00000000', 'digilines:wire_std_00000000', 'digilines:wire_std_00000000'},
+			}
+		})
+	end
+
+	technic.register_cable("LV", 2/16, S("LV Cable (digiline)"), "_digi", {
+		digiline = {
+			wire = {
+				rules = {
+					{x = 1, y = 0, z = 0},
+					{x =-1, y = 0, z = 0},
+					{x = 0, y = 1, z = 0},
+					{x = 0, y =-1, z = 0},
+					{x = 0, y = 0, z = 1},
+					{x = 0, y = 0, z =-1}
+				}
+			}
+		}
+	})
+end
