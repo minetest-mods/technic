minetest-mods/technic

commit 0f6bdb1bde5f476b8e4500cba559f813cd5276a1

Correct (almost) all luacheck warnings
Also includes a few code opimizations:
- Combined mining drill functions
- Shorter alias registration (unused?)
- Add TODOs to address later

technic/tools/mining_drill.lua
@@ -239,97 +239,61 @@ local function pos_is_pointable(pos)
 	return nodedef and nodedef.pointable
 end
 
-local function mining_drill_mk2_setmode(user,itemstack)
-	local player_name=user:get_player_name()
-	local item=itemstack:to_table()
-	local mode = nil
-	local meta=minetest.deserialize(item["metadata"])
-	if meta==nil then
-		meta={}
-		mode=0
+local function mining_drill_mkX_setmode(user, itemstack, drill_type, max_modes)
+	local player_name = user:get_player_name()
+	local meta = minetest.deserialize(itemstack:get_metadata()) or {}
+
+	if not meta["mode"] then
+		minetest.chat_send_player(player_name,
+			S("Use while sneaking to change Mining Drill Mk%d modes."):format(drill_type))
 	end
-	if meta["mode"]==nil then
-		minetest.chat_send_player(player_name, S("Use while sneaking to change Mining Drill Mk%d modes."):format(2))
-		meta["mode"]=0
-		mode=0
-	end
-	mode=(meta["mode"])
-	mode=mode+1
-	if mode>=5 then mode=1 end
-	minetest.chat_send_player(player_name, S("Mining Drill Mk%d Mode %d"):format(2, mode)..": "..mining_drill_mode_text[mode][1])
-    itemstack:set_name("technic:mining_drill_mk2_"..mode);
-	meta["mode"]=mode
+	local mode = (meta["mode"] or 0) + 1
+	if mode > max_modes then mode = 1 end
+
+	minetest.chat_send_player(player_name,
+		S("Mining Drill Mk%d Mode %d"):format(2, mode)..
+		": "..mining_drill_mode_text[mode][1])
+    itemstack:set_name(("technic:mining_drill_mk%d_%s"):format(drill_type, mode))
+	meta["mode"] = mode
     itemstack:set_metadata(minetest.serialize(meta))
 	return itemstack
 end
 
-local function mining_drill_mk3_setmode(user,itemstack)
-	local player_name=user:get_player_name()
-	local item=itemstack:to_table()
-	local meta=minetest.deserialize(item["metadata"])
-	if meta==nil then
-		meta={}
-		mode=0
-	end
-	if meta["mode"]==nil then
-		minetest.chat_send_player(player_name, S("Use while sneaking to change Mining Drill Mk%d modes."):format(3))
-		meta["mode"]=0
-		mode=0
-	end
-	mode=(meta["mode"])
-	mode=mode+1
-	if mode>=6 then mode=1 end
-	minetest.chat_send_player(player_name, S("Mining Drill Mk%d Mode %d"):format(3, mode)..": "..mining_drill_mode_text[mode][1])
-    itemstack:set_name("technic:mining_drill_mk3_"..mode);
-	meta["mode"]=mode
-    itemstack:set_metadata(minetest.serialize(meta))
-	return itemstack
-end
-
-
-local function mining_drill_mk2_handler(itemstack, user, pointed_thing)
+local function mining_drill_mkX_handler(itemstack, user, pointed_thing, drill_type, max_modes)
 	local keys = user:get_player_control()
-	local meta = minetest.deserialize(itemstack:get_metadata())
-	if not meta or not meta.mode or keys.sneak then
-		return mining_drill_mk2_setmode(user, itemstack)
-	end
-	if pointed_thing.type ~= "node" or not pos_is_pointable(pointed_thing.under) or not meta.charge then
-		return
-	end
-	local charge_to_take = cost_to_use(2, meta.mode)
-	if meta.charge >= charge_to_take then
-		local pos = minetest.get_pointed_thing_position(pointed_thing, false)
-		drill_dig_it(pos, user, meta.mode)
-		if not technic.creative_mode then
-			meta.charge = meta.charge - charge_to_take
-			itemstack:set_metadata(minetest.serialize(meta))
-			technic.set_RE_wear(itemstack, meta.charge, max_charge[2])
+	local meta = minetest.deserialize(itemstack:get_metadata()) or {}
+
+	-- Mode switching (if possible)
+	if max_modes > 1 then
+		if not meta.mode or keys.sneak then
+			return mining_drill_mkX_setmode(user, itemstack, drill_type, max_modes)
 		end
 	end
+	if pointed_thing.type ~= "node" or not pos_is_pointable(pointed_thing.under) then
+		return
+	end
+	if not meta.charge then
+		return
+	end
+
+	-- Check whether the tool has enough charge
+	local charge_to_take = cost_to_use(drill_type, meta.mode or 1)
+	if meta.charge < charge_to_take then
+		return
+	end
+
+	-- Do the actual shoorting action
+	local pos = minetest.get_pointed_thing_position(pointed_thing, false)
+	drill_dig_it(pos, user, meta.mode)
+	if not technic.creative_mode then
+		meta.charge = meta.charge - charge_to_take
+		itemstack:set_metadata(minetest.serialize(meta))
+		technic.set_RE_wear(itemstack, meta.charge, max_charge[drill_type])
+	end
 	return itemstack
 end
 
-local function mining_drill_mk3_handler(itemstack, user, pointed_thing)
-	local keys = user:get_player_control()
-	local meta = minetest.deserialize(itemstack:get_metadata())
-	if not meta or not meta.mode or keys.sneak then
-		return mining_drill_mk3_setmode(user, itemstack)
-	end
-	if pointed_thing.type ~= "node" or not pos_is_pointable(pointed_thing.under) or not meta.charge then
-		return
-	end
-	local charge_to_take = cost_to_use(3, meta.mode)
-	if meta.charge >= charge_to_take then
-		local pos = minetest.get_pointed_thing_position(pointed_thing, false)
-		drill_dig_it(pos, user, meta.mode)
-		if not technic.creative_mode then
-			meta.charge = meta.charge - charge_to_take
-			itemstack:set_metadata(minetest.serialize(meta))
-			technic.set_RE_wear(itemstack, meta.charge, max_charge[3])
-		end
-	end
-	return itemstack
-end
+-- Simple mining drill registration
 
 technic.register_power_tool("technic:mining_drill", max_charge[1])
 
@@ -340,26 +304,12 @@ minetest.register_tool("technic:mining_drill", {
 	wear_represents = "technic_RE_charge",
 	on_refill = technic.refill_RE_charge,
 	on_use = function(itemstack, user, pointed_thing)
-		if pointed_thing.type ~= "node" or not pos_is_pointable(pointed_thing.under) then
-			return itemstack
-		end
-		local meta = minetest.deserialize(itemstack:get_metadata())
-		if not meta or not meta.charge then
-			return
-		end
-		local charge_to_take = cost_to_use(1, 1)
-		if meta.charge >= charge_to_take then
-			local pos = minetest.get_pointed_thing_position(pointed_thing, false)
-			drill_dig_it(pos, user, 1)
-			if not technic.creative_mode then
-				meta.charge = meta.charge - charge_to_take
-				itemstack:set_metadata(minetest.serialize(meta))
-				technic.set_RE_wear(itemstack, meta.charge, max_charge[1])
-			end
-		end
+		mining_drill_mkX_handler(itemstack, user, pointed_thing, 1, 1)
 		return itemstack
 	end,
 })
+
+-- Mk2 registration
 
 minetest.register_tool("technic:mining_drill_mk2", {
 	description = S("Mining Drill Mk%d"):format(2),
@@ -367,7 +317,7 @@ minetest.register_tool("technic:mining_drill_mk2", {
 	wear_represents = "technic_RE_charge",
 	on_refill = technic.refill_RE_charge,
 	on_use = function(itemstack, user, pointed_thing)
-		mining_drill_mk2_handler(itemstack, user, pointed_thing)
+		mining_drill_mkX_handler(itemstack, user, pointed_thing, 2, 4)
 		return itemstack
 	end,
 })
@@ -384,11 +334,13 @@ for i = 1, 4 do
 		on_refill = technic.refill_RE_charge,
 		groups = {not_in_creative_inventory=1},
 		on_use = function(itemstack, user, pointed_thing)
-			mining_drill_mk2_handler(itemstack, user, pointed_thing)
+			mining_drill_mkX_handler(itemstack, user, pointed_thing, 2, 4)
 			return itemstack
 		end,
 	})
 end
+
+-- Mk3 registration
 
 minetest.register_tool("technic:mining_drill_mk3", {
 	description = S("Mining Drill Mk%d"):format(3),
@@ -396,8 +348,8 @@ minetest.register_tool("technic:mining_drill_mk3", {
 	wear_represents = "technic_RE_charge",
 	on_refill = technic.refill_RE_charge,
 	on_use = function(itemstack, user, pointed_thing)
-	mining_drill_mk3_handler(itemstack,user,pointed_thing)
-	return itemstack
+		mining_drill_mkX_handler(itemstack, user, pointed_thing, 3, 5)
+		return itemstack
 	end,
 })
 
@@ -413,8 +365,8 @@ for i=1,5,1 do
 		on_refill = technic.refill_RE_charge,
 		groups = {not_in_creative_inventory=1},
 		on_use = function(itemstack, user, pointed_thing)
-		mining_drill_mk3_handler(itemstack,user,pointed_thing)
-		return itemstack
+			mining_drill_mkX_handler(itemstack, user, pointed_thing, 3, 5)
+			return itemstack
 		end,
 	})
 end

.luacheckrc
@@ -1,6 +1,8 @@
 unused_args = false
 allow_defined_top = true
-max_line_length = 999
+max_line_length = 150
+-- Allow shadowed variables (callbacks in callbacks)
+redefined = false
 
 globals = {
     "technic", "minetest",
@@ -32,19 +34,7 @@ read_globals = {
     "craftguide", "i3"
 }
 
-files["concrete/init.lua"].ignore = { "steel_ingot" }
-files["technic/machines/MV/tool_workshop.lua"].ignore = { "pos" }
-files["technic/machines/other/frames.lua"].ignore = { "item_texture", "item_type", "adj", "connected", "" }
-files["technic/machines/register/battery_box.lua"].ignore = { "pos", "tube_upgrade" }
-files["technic/machines/register/cables.lua"].ignore = { "name", "from_below", "p" }
-files["technic/machines/register/common.lua"].ignore = { "result" }
-
-files["technic/machines/register/generator.lua"].ignore = { "node" }
-files["technic/machines/switching_station.lua"].ignore = { "pos1", "tier", "poshash" }
-files["technic/radiation.lua"].ignore = { "LAVA_VISC" }
-files["technic/tools/chainsaw.lua"].ignore = { "pos" }
-files["technic/tools/mining_drill.lua"].ignore = { "mode" }
-files["technic_chests/register.lua"].ignore = { "fs_helpers", "name", "locked_after_place" }
-
-files["technic_cnc/cnc.lua"].ignore = { "multiplier" }
-files["wrench/init.lua"].ignore = { "name", "stack" }
+-- Loop warning
+files["technic/machines/other/frames.lua"].ignore = { "" }
+-- Long lines
+files["technic_cnc/cnc_api.lua"].ignore = { "" }

concrete/init.lua
@@ -16,13 +16,6 @@ for i = 32, 63 do
 			"technic:concrete_post_with_platform")
 end
 
-local steel_ingot
-if minetest.get_modpath("technic_worldgen") then
-	steel_ingot = "technic:carbon_steel_ingot"
-else
-	steel_ingot = "default:steel_ingot"
-end
-
 minetest.register_craft({
 	output = 'technic:concrete_post_platform 6',
 	recipe = {

extranodes/init.lua
@@ -57,36 +57,47 @@ if minetest.get_modpath("moreblocks") then
 		tiles={"technic_stainless_steel_block.png"},
 	})
 
-	function register_technic_stairs_alias(modname, origname, newmod, newname)
-		minetest.register_alias(modname .. ":slab_" .. origname, newmod..":slab_" .. newname)
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_inverted", newmod..":slab_" .. newname .. "_inverted")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_wall", newmod..":slab_" .. newname .. "_wall")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_quarter", newmod..":slab_" .. newname .. "_quarter")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_quarter_inverted", newmod..":slab_" .. newname .. "_quarter_inverted")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_quarter_wall", newmod..":slab_" .. newname .. "_quarter_wall")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_three_quarter", newmod..":slab_" .. newname .. "_three_quarter")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_three_quarter_inverted", newmod..":slab_" .. newname .. "_three_quarter_inverted")
-		minetest.register_alias(modname .. ":slab_" .. origname .. "_three_quarter_wall", newmod..":slab_" .. newname .. "_three_quarter_wall")
-		minetest.register_alias(modname .. ":stair_" .. origname, newmod..":stair_" .. newname)
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_inverted", newmod..":stair_" .. newname .. "_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_wall", newmod..":stair_" .. newname .. "_wall")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_wall_half", newmod..":stair_" .. newname .. "_wall_half")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_wall_half_inverted", newmod..":stair_" .. newname .. "_wall_half_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_half", newmod..":stair_" .. newname .. "_half")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_half_inverted", newmod..":stair_" .. newname .. "_half_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_right_half", newmod..":stair_" .. newname .. "_right_half")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_right_half_inverted", newmod..":stair_" .. newname .. "_right_half_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_wall_half", newmod..":stair_" .. newname .. "_wall_half")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_wall_half_inverted", newmod..":stair_" .. newname .. "_wall_half_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_inner", newmod..":stair_" .. newname .. "_inner")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_inner_inverted", newmod..":stair_" .. newname .. "_inner_inverted")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_outer", newmod..":stair_" .. newname .. "_outer")
-		minetest.register_alias(modname .. ":stair_" .. origname .. "_outer_inverted", newmod..":stair_" .. newname .. "_outer_inverted")
-		minetest.register_alias(modname .. ":panel_" .. origname .. "_bottom", newmod..":panel_" .. newname .. "_bottom")
-		minetest.register_alias(modname .. ":panel_" .. origname .. "_top", newmod..":panel_" .. newname .. "_top")
-		minetest.register_alias(modname .. ":panel_" .. origname .. "_vertical", newmod..":panel_" .. newname .. "_vertical")
-		minetest.register_alias(modname .. ":micro_" .. origname .. "_bottom", newmod..":micro_" .. newname .. "_bottom")
-		minetest.register_alias(modname .. ":micro_" .. origname .. "_top", newmod..":micro_" .. newname .. "_top")
+	function register_technic_stairs_alias(origmod, origname, newmod, newname)
+		local func = minetest.register_alias
+		local function remap(kind, suffix)
+			-- Old: stairsplus:slab_concrete_wall
+			-- New:    technic:slab_concrete_wall
+			func(("%s:%s_%s%s"):format(origmod, kind, origname, suffix),
+				("%s:%s_%s%s"):format(newmod, kind, newname, suffix))
+		end
+
+		-- Slabs
+		remap("slab", "")
+		remap("slab", "_inverted")
+		remap("slab", "_wall")
+		remap("slab", "_quarter")
+		remap("slab", "_quarter_inverted")
+		remap("slab", "_quarter_wall")
+		remap("slab", "_three_quarter")
+		remap("slab", "_three_quarter_inverted")
+		remap("slab", "_three_quarter_wall")
+
+		-- Stairs
+		remap("stair", "")
+		remap("stair", "_inverted")
+		remap("stair", "_wall")
+		remap("stair", "_wall_half")
+		remap("stair", "_wall_half_inverted")
+		remap("stair", "_half")
+		remap("stair", "_half_inverted")
+		remap("stair", "_right_half")
+		remap("stair", "_right_half_inverted")
+		remap("stair", "_inner")
+		remap("stair", "_inner_inverted")
+		remap("stair", "_outer")
+		remap("stair", "_outer_inverted")
+
+		-- Other
+		remap("panel", "_bottom")
+		remap("panel", "_top")
+		remap("panel", "_vertical")
+		remap("micro", "_bottom")
+		remap("micro", "_top")
 	end
 
 	register_technic_stairs_alias("stairsplus", "concrete", "technic", "concrete")

technic/init.lua
@@ -16,7 +16,17 @@ technic.modpath = modpath
 if rawget(_G, "intllib") then
 	technic.getter = intllib.Getter()
 else
-	technic.getter = function(s,a,...)if a==nil then return s end a={a,...}return s:gsub("(@?)@(%(?)(%d+)(%)?)",function(e,o,n,c)if e==""then return a[tonumber(n)]..(o==""and c or"")else return"@"..o..n..c end end) end
+	-- Intllib copypasta: TODO replace with the client-side translation API
+	technic.getter = function(s,a,...)
+		if a==nil then return s end
+		a={a,...}
+		return s:gsub("(@?)@(%(?)(%d+)(%)?)", function(e,o,n,c)
+			if e==""then
+				return a[tonumber(n)]..(o==""and c or"")
+			end
+			return "@"..o..n..c
+		end)
+	end
 end
 local S = technic.getter
 

technic/machines/HV/forcefield.lua
@@ -112,10 +112,13 @@ local function set_forcefield_formspec(meta)
 	else
 		formspec = formspec.."button[0,1;5,1;mesecon_mode_0;"..S("Controlled by Mesecon Signal").."]"
 	end
+	-- TODO: String replacement with %s will stop working with client-side translations
 	if meta:get_int("enabled") == 0 then
-		formspec = formspec.."button[0,1.75;5,1;enable;"..S("%s Disabled"):format(S("%s Forcefield Emitter"):format("HV")).."]"
+		formspec = formspec.."button[0,1.75;5,1;enable;"..
+			S("%s Disabled"):format(S("%s Forcefield Emitter"):format("HV")).."]"
 	else
-		formspec = formspec.."button[0,1.75;5,1;disable;"..S("%s Enabled"):format(S("%s Forcefield Emitter"):format("HV")).."]"
+		formspec = formspec.."button[0,1.75;5,1;disable;"..
+			S("%s Enabled"):format(S("%s Forcefield Emitter"):format("HV")).."]"
 	end
 	meta:set_string("formspec", formspec)
 end

technic/machines/HV/quarry.lua
@@ -47,14 +47,19 @@ local function set_quarry_demand(meta)
 	local radius = meta:get_int("size")
 	local diameter = radius*2 + 1
 	local machine_name = S("%s Quarry"):format("HV")
-	if meta:get_int("enabled") == 0 or meta:get_int("purge_on") == 1 then
-		meta:set_string("infotext", S(meta:get_int("purge_on") == 1 and "%s purging cache" or "%s Disabled"):format(machine_name))
+	local do_purge = meta:get_int("purge_on") == 1
+	if meta:get_int("enabled") == 0 or do_purge then
+		local infotext = do_purge and
+			S("%s purging cache") or S("%s Disabled")
+		meta:set_string("infotext", infotext:format(machine_name))
 		meta:set_int("HV_EU_demand", 0)
 	elseif meta:get_int("dug") == diameter*diameter * (quarry_dig_above_nodes+1+quarry_max_depth) then
 		meta:set_string("infotext", S("%s Finished"):format(machine_name))
 		meta:set_int("HV_EU_demand", 0)
 	else
-		meta:set_string("infotext", S(meta:get_int("HV_EU_input") >= quarry_demand and "%s Active" or "%s Unpowered"):format(machine_name))
+		local infotext = meta:get_int("HV_EU_input") >= quarry_demand
+			and S("%s Active") or S("%s Unpowered")
+		meta:set_string("infotext", infotext:format(machine_name))
 		meta:set_int("HV_EU_demand", quarry_demand)
 	end
 end

technic/machines/MV/power_radiator.lua
@@ -36,7 +36,8 @@ technic.register_inductive_machine = function(name)
 end
 
 -- Appliances:
---  has_supply: pos of supply node if the appliance has a power radiator near with sufficient power for the demand else ""
+--  has_supply: pos of supply node if the appliance has a power radiator near
+--              with sufficient power for the demand else ""
 --  EU_demand: The power demand of the device.
 --  EU_charge: Actual use. set to EU_demand if active==1
 --  active: set to 1 if the device is on

commit bce5306abea765fed1fdfeb775ed3abd21ff2863

Fix Mining Drill Mk1 not drilling
Problem introduced in  0f6bdb1

technic/tools/mining_drill.lua
@@ -46,20 +46,27 @@ local mining_drill_mode_text = {
 	{S("3x3 nodes.")},
 }
 
-local function drill_dig_it0 (pos,player)
+local function drill_dig_it0(pos, player)
 	if minetest.is_protected(pos, player:get_player_name()) then
 		minetest.record_protection_violation(pos, player:get_player_name())
 		return
 	end
 	local node = minetest.get_node(pos)
-	if node.name == "air" or node.name == "ignore" then return end
-	if node.name == "default:lava_source" then return end
-	if node.name == "default:lava_flowing" then return end
-	if node.name == "default:water_source" then minetest.remove_node(pos) return end
-	if node.name == "default:water_flowing" then minetest.remove_node(pos) return end
-	local def = minetest.registered_nodes[node.name]
-	if not def then return end
-	def.on_dig(pos, node, player)
+	local ndef = minetest.registered_nodes[node.name]
+	if not ndef or ndef.drawtype == "airlike" then
+		-- Covers "air", "ignore", unknown nodes and more.
+		return
+	end
+	local groups = ndef and ndef.groups or {}
+
+	if groups.lava then
+		return
+	end
+	if groups.water then
+		minetest.remove_node(pos)
+		return
+	end
+	ndef.on_dig(pos, node, player)
 end
 
 local function drill_dig_it1 (player)
@@ -284,7 +291,7 @@ local function mining_drill_mkX_handler(itemstack, user, pointed_thing, drill_ty
 
 	-- Do the actual shoorting action
 	local pos = minetest.get_pointed_thing_position(pointed_thing, false)
-	drill_dig_it(pos, user, meta.mode)
+	drill_dig_it(pos, user, meta.mode or 1)
 	if not technic.creative_mode then
 		meta.charge = meta.charge - charge_to_take
 		itemstack:set_metadata(minetest.serialize(meta))