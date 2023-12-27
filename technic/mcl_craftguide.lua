8857680d29124c8341daaa9fd3be4c65143b3293
James David Clarke <james@jamesdavidclarke.com>
2023-12-27 02:48:43 +0000

    Add alloy recipes to mcl_craftrecipes

--- technic/mcl_craftguide.lua
+++ technic/mcl_craftguide.lua
@@ -24,6 +24,11 @@ mcl_craftguide.register_craft_type("grinder", {
     icon = "technic_lv_grinder_front_active.png", 
 })
 
+mcl_craftguide.register_craft_type("alloy_furnace", {
+    description = "Alloy Furnace",
+    icon = "technic_coal_alloy_furnace_front_active.png",
+})
+
 
 centrifuge_recipes = {
 	{ "technic:bronze_dust 8",             "technic:copper_dust 7",       "technic:tin_dust"      },
@@ -93,6 +98,35 @@ grinder_recipes = {
     {desert_stone_ingrediant,   desert_sand_ingrediant.." 2"}, -- reverse recipe can be found in the compressor
     {ice_block_ingrediant,         snow_block_ingrediant},
 }
+
+alloy_recipes = {
+	{"technic:copper_dust 7",         "technic:tin_dust",           "technic:bronze_dust 8", 12},
+	{copper_ingrediant.." 7",        tin_ingrediant,          bronze_ingrediant.." 8", 12},
+	{"technic:wrought_iron_dust 2",   "technic:coal_dust",          "technic:carbon_steel_dust 2", 6},
+	{"technic:wrought_iron_ingot 2",  "technic:coal_dust",          "technic:carbon_steel_ingot 2", 6},
+	{"technic:carbon_steel_dust 2",   "technic:coal_dust",          "technic:cast_iron_dust 2", 6},
+	{"technic:carbon_steel_ingot 2",  "technic:coal_dust",          "technic:cast_iron_ingot 2", 6},
+	{"technic:carbon_steel_dust 4",   "technic:chromium_dust",      "technic:stainless_steel_dust 5", 7.5},
+	{"technic:carbon_steel_ingot 4",  "technic:chromium_ingot",     "technic:stainless_steel_ingot 5", 7.5},
+	{"technic:copper_dust 2",         "technic:zinc_dust",          "technic:brass_dust 3"},
+	{copper_ingrediant.." 2",        "technic:zinc_ingot",         "basic_materials:brass_ingot 3"},
+	{sand_ingrediant.." 2",                "technic:coal_dust 2",        "technic:silicon_wafer"},
+	{"technic:silicon_wafer",         "technic:gold_dust",          "technic:doped_silicon_wafer"},
+	-- from https://en.wikipedia.org/wiki/Carbon_black
+	-- The highest volume use of carbon black is as a reinforcing filler in rubber products, especially tires.
+	-- "[Compounding a] pure gum vulcanizate … with 50% of its weight of carbon black improves its tensile strength and wear resistance …"
+	{"technic:raw_latex 4",           "technic:coal_dust 2",        "technic:rubber 6", 2},
+	{ice_block_ingrediant, 		  emtpy_bucket_ingrediant,        water_bucket_ingrediant, 1 },
+}
+
+for _, data in pairs(alloy_recipes) do
+	mcl_craftguide.register_craft({
+        type = "alloy_furnace",
+        width = 1,
+        output = data[3],
+        items = {data[1], data[2]},
+    })
+end
 
 -- Register Centrifuge Recipes
 for _, data in pairs(centrifuge_recipes) do
