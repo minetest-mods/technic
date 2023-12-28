16b270c6e7c7a806a734b6c90c113176448a97fa
James David Clarke <james@jamesdavidclarke.com>
2023-12-28 15:53:59 +0000

    Fixed Water mill code

--- technic/machines/LV/water_mill.lua
+++ technic/machines/LV/water_mill.lua
@@ -19,15 +19,7 @@ minetest.register_craft({
 
 local function check_node_around_mill(pos)
 	local node = minetest.get_node(pos)
-	if technic_compat.mcl then
-		local water_flowing_node = "mcl_core:water_flowing"
-		local river_water_flowing_node = "mclx_core:river_water_flowing"
-	else
-		local water_flowing_node = "default:water_flowing"
-		local river_water_flowing_node = "default:river_water_flowing"
-	end
-	if node.name == water_flowing_node
-	  or node.name == river_water_flowing_node then
+	if minetest.get_item_group(node.name, "water") == 3 and string.find(node.name, "flowing") then
 		return node.param2 -- returns approx. water flow, if any
 	end
 	return false
