e57dc5b16cc36ec5bc6d7777e6a3aff8ba95c10e
gabriel1379 <gabriel1379@gmail.com>
2024-03-09 15:00:06 +0100

    Add everness sandstone compressor recipes, clear their default recipes

--- technic/machines/register/compressor_recipes.lua
+++ technic/machines/register/compressor_recipes.lua
@@ -21,6 +21,17 @@ local recipes = {
 	{"technic:uranium35_ingot 5",  "technic:uranium_fuel"},
 }
 
+if minetest.get_modpath("everness") then
+	table.insert(recipes, {"everness:coral_deep_ocean_sand 2",			"everness:coral_deep_ocean_sandstone_block"})
+	table.insert(recipes, {"everness:coral_sand 2",						"everness:coral_sandstone"})
+	table.insert(recipes, {"everness:coral_white_sand 2",				"everness:coral_white_sandstone"})
+	table.insert(recipes, {"everness:crystal_forest_deep_ocean_sand 2", "everness:crystal_forest_deep_ocean_sandstone_block"})
+	table.insert(recipes, {"everness:crystal_sand 2",					"everness:crystal_sandstone"})
+	table.insert(recipes, {"everness:cursed_lands_deep_ocean_sand 2",	"everness:cursed_lands_deep_ocean_sandstone_block"})
+	table.insert(recipes, {"everness:cursed_sand 2",					"everness:cursed_sandstone_block"})
+	table.insert(recipes, {"everness:mineral_sand 2",					"everness:mineral_sandstone"})
+end
+
 -- defuse the default sandstone recipe, since we have the compressor to take over in a more realistic manner
 minetest.clear_craft({
 	recipe = {
@@ -40,6 +51,57 @@ minetest.clear_craft({
 		{"default:silver_sand", "default:silver_sand"},
 	},
 })
+
+if minetest.get_modpath("everness") then
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:coral_sand', 'everness:coral_sand' },
+        	{ 'everness:coral_sand', 'everness:coral_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:coral_forest_deep_ocean_sand', 'everness:coral_forest_deep_ocean_sand' },
+        	{ 'everness:coral_forest_deep_ocean_sand', 'everness:coral_forest_deep_ocean_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:coral_white_sand', 'everness:coral_white_sand' },
+        	{ 'everness:coral_white_sand', 'everness:coral_white_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:crystal_sand', 'everness:crystal_sand' },
+        	{ 'everness:crystal_sand', 'everness:crystal_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:cursed_sand', 'everness:cursed_sand' },
+        	{ 'everness:cursed_sand', 'everness:cursed_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:cursed_lands_deep_ocean_sand', 'everness:cursed_lands_deep_ocean_sand' },
+        	{ 'everness:cursed_lands_deep_ocean_sand', 'everness:cursed_lands_deep_ocean_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:crystal_forest_deep_ocean_sand', 'everness:crystal_forest_deep_ocean_sand' },
+        	{ 'everness:crystal_forest_deep_ocean_sand', 'everness:crystal_forest_deep_ocean_sand' },
+		},
+	})
+	minetest.clear_craft({
+		recipe = {
+			{ 'everness:mineral_sand', 'everness:mineral_sand' },
+        	{ 'everness:mineral_sand', 'everness:mineral_sand' },
+		},
+	})
+end
 
 for _, data in pairs(recipes) do
 	technic.register_compressor_recipe({input = {data[1]}, output = data[2]})
