d65c4aadd233ff89705e109c15685570b52909c3
Gábriel <38207624+gabriel1379@users.noreply.github.com>
2024-05-06 17:31:13 +0200

    Add grinder crafting recipe from everness desert stone (#637)
    Add two alternative grinder crafting recipes with the two everness desert stones

--- technic/machines/LV/grinder.lua
+++ technic/machines/LV/grinder.lua
@@ -9,5 +9,25 @@ minetest.register_craft({
 	}
 })
 
+if (minetest.get_modpath('everness')) then
+	minetest.register_craft({
+		output = 'technic:lv_grinder',
+		recipe = {
+			{'everness:coral_desert_stone', 'default:diamond',        'everness:coral_desert_stone'},
+			{'everness:coral_desert_stone', 'technic:machine_casing', 'everness:coral_desert_stone'},
+			{'technic:granite',             'technic:lv_cable',       'technic:granite'},
+		}
+	})
+
+	minetest.register_craft({
+		output = 'technic:lv_grinder',
+		recipe = {
+			{'everness:forsaken_desert_stone', 'default:diamond',        'everness:forsaken_desert_stone'},
+			{'everness:forsaken_desert_stone', 'technic:machine_casing', 'everness:forsaken_desert_stone'},
+			{'technic:granite',                'technic:lv_cable',       'technic:granite'},
+		}
+	})
+end
+
 technic.register_grinder({tier="LV", demand={200}, speed=1})
 
