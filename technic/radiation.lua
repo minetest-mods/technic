f9026bfacf2651c2d112cdf8bb6b1914c253fc5b
DustyDave961 <144002335+DustyDave961@users.noreply.github.com>
2024-10-19 06:59:22 -0500

    Update radiation.lua
    Why is whitespace such an issue? There's nothing there.

--- technic/radiation.lua
+++ technic/radiation.lua
@@ -197,7 +197,7 @@ local function node_radiation_resistance(node_name)
 	if def.groups.rad_resistance then
 		resistance = resistance + def.groups.rad_resistance
 	end
-   
+	
 	return math.sqrt(resistance)
 end
 
