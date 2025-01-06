a98870968ab5668c9461b7d7a5a6e2298e7dd510
Vitaliy Olkhin <ovvitalik@gmail.com>
2025-01-06 21:12:53 +0500

    deleted old code

--- technic/init.lua
+++ technic/init.lua
@@ -1,38 +1,11 @@
 -- namespace: technic
 -- (c) 2012-2013 by RealBadAngel <mk@realbadangel.pl>
 
-if not minetest.get_translator then
-	error("[technic] Your Minetest version is no longer supported."
-		.. " (version < 5.0.0)")
-end
-
-local load_start = os.clock()
-
 technic = rawget(_G, "technic") or {}
 technic.creative_mode = minetest.settings:get_bool("creative_mode")
 
-
 local modpath = minetest.get_modpath("technic")
 technic.modpath = modpath
-
-
--- Boilerplate to support intllib
-if rawget(_G, "intllib") then
-	technic.getter = intllib.Getter()
-else
-	-- Intllib copypasta: TODO replace with the client-side translation API
-	technic.getter = function(s,a,...)
-		if a==nil then return s end
-		a={a,...}
-		return s:gsub("(@?)@(%(?)(%d+)(%)?)", function(e,o,n,c)
-			if e==""then
-				return a[tonumber(n)]..(o==""and c or"")
-			end
-			return "@"..o..n..c
-		end)
-	end
-end
-local S = minetest.get_translator("technic")
 
 -- Read configuration file
 dofile(modpath.."/config.lua")
@@ -60,8 +33,4 @@ dofile(modpath.."/tools/init.lua")
 
 -- Aliases for legacy node/item names
 dofile(modpath.."/legacy.lua")
-
-if minetest.settings:get_bool("log_mods") then
-	print(S("[Technic] Loaded in @1 seconds", os.clock() - load_start))
-end
 
