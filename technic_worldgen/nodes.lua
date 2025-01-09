4d7f6e870bf8fe87fd9a0d892ac6231c90fabf44
Vitaliy Olkhin <ovvitalik@gmail.com>
2025-01-09 19:01:06 +0500

    update translate

--- technic_worldgen/nodes.lua
+++ technic_worldgen/nodes.lua
@@ -178,7 +178,7 @@ for_each_registered_node(function(node_name, node_def)
 			node_name:find("steelblock", 1, true) and
 			node_def.description:find("Steel", 1, true) then
 		minetest.override_item(node_name, {
-			description = node_def.description:gsub("Steel", tr_wrought),
+			description = "Steel".. tr_wrought,
 		})
 	end
 	local tiles = node_def.tiles or node_def.tile_images
