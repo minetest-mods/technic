9414584946c04a8f9e1b7f92f8df87aca641cfed
SmallJoker <mk939@ymail.com>
2026-07-07 19:18:38 +0200

    technic_cnc: Unify access checks and machine translation

--- technic_cnc/init.lua
+++ technic_cnc/init.lua
@@ -8,6 +8,14 @@ technic_cnc.getter = minetest.get_translator("technic_cnc")
 technic_cnc.use_technic = technic_cnc.technic_modpath
                           and minetest.settings:get_bool("technic_cnc_use_technic") ~= false
 
+-- From technic/helpers.lua
+local S = technic_cnc.getter
+function technic_cnc._get_desc_formatter(name, status)
+	return function(status)
+		return status and S("@1@nStatus: @2", name, status) or name
+	end
+end
+
 
 dofile(modpath.."/cnc.lua")
 dofile(modpath.."/cnc_api.lua")
